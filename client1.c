6f47922a748010d17e4366b25c02cfaf3561276d
jakubbutryn <113950093+jakubbutryn@users.noreply.github.com>
2022-09-27 02:36:46 +0200

    Update client1.c

--- client1.c
+++ client1.c
@@ -14,21 +14,21 @@ exit(0);
 }
 int main(int argc, char *argv[])
 {
-int sockfd, portno, n; // define ints
-struct sockaddr_in serv_addr; // define structs
-struct hostent *server; // defecines host address struct
-char buffer[256]; // define buffer
+int sockfd, portno, n; 
+struct sockaddr_in serv_addr; 
+struct hostent *server;
+char buffer[256]; 
 if (argc < 3) {
 fprintf(stderr,"usage %s hostname port\n", argv[0]);
 exit(0); // exit main() function
 }
-portno = atoi(argv[2]); // get port number
-sockfd = socket(AF_INET, SOCK_STREAM, 0); // create socket
+portno = atoi(argv[2]); 
+sockfd = socket(AF_INET, SOCK_STREAM, 0); 
 if (sockfd < 0)
-error("ERROR opening socket"); // if socket creation failed
-server = gethostbyname(argv[1]); // get server address
+error("ERROR opening socket");
+server = gethostbyname(argv[1]);
 if (server == NULL) {
-fprintf(stderr,"ERROR, no such host\n"); // if server address is not proper
+fprintf(stderr,"ERROR, no such host\n"); 
 exit(0); // exit main() function
 }
 bzero((char *) &serv_addr, sizeof(serv_addr));
@@ -39,19 +39,19 @@ server->h_length); // copy byte data
 serv_addr.sin_port = htons(portno); // assign port
 
 //if (connect(sockfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0)
-//error("ERROR connecting"); // if connection failed
+//error("ERROR connecting"); 
 
-printf("Please enter the message: "); //
-bzero(buffer,256); // fill buffer with zeros
-fgets(buffer,255,stdin); // read into buffer
-n = write(sockfd,buffer,strlen(buffer)); // write the msg to socket
+printf("Please enter the message: "); 
+bzero(buffer,256); 
+fgets(buffer,255,stdin);
+n = write(sockfd,buffer,strlen(buffer)); 
 if (n < 0)
-error("ERROR writing to socket"); // writing failed
-bzero(buffer,256); // if writing succeed, zero the buffer
-n = read(sockfd,buffer,255); // read from socket into buffer
+error("ERROR writing to socket");
+bzero(buffer,256);
+n = read(sockfd,buffer,255); 
 if (n < 0)
-error("ERROR reading from socket"); // if reading failed
-printf("%s\n",buffer); // print buffer
-close(sockfd); // close socket
-return 0; // exit main() function
+error("ERROR reading from socket");
+printf("%s\n",buffer); 
+close(sockfd); 
+return 0; 
 }
